d504655564fc69a5b6e6c3fb0d632f5670a6fc8a
Ninoh-FOX <ninohfox@gmail.com>
2020-12-12 10:22:05 +0100

    Update board-gcw0.c

--- arch/mips/jz4770/board-gcw0.c
+++ arch/mips/jz4770/board-gcw0.c
@@ -110,7 +110,7 @@ static void gcw0_panel_disable(void *panel)
 static struct rg280_platform_data gcw0_panel_pdata = {
 	.gpio_reset		= JZ_GPIO_PORTE(2),
 	.gpio_clock		= JZ_GPIO_PORTE(15),
-	.gpio_enable	= JZ_GPIO_PORTE(16),
+	.gpio_enable		= JZ_GPIO_PORTE(16),
 	.gpio_data		= JZ_GPIO_PORTE(17),
 };
 
@@ -123,7 +123,7 @@ static struct panel_ops gcw0_panel_ops = {
 
 static struct jzfb_platform_data gcw0_fb_pdata = {
 	.panel_ops		= &gcw0_panel_ops,
-	.panel_pdata	= &gcw0_panel_pdata,
+	.panel_pdata		= &gcw0_panel_pdata,
 };
 
 
@@ -339,7 +339,7 @@ static struct jz_battery_platform_data gcw0_battery_pdata = {
 	.info = {
 		.name = "battery",
 		.technology = POWER_SUPPLY_TECHNOLOGY_LIPO,
-		.voltage_max_design = 4100000,
+		.voltage_max_design = 4200000,
 		.voltage_min_design = 3310000,
 	},
 };
@@ -555,7 +555,7 @@ static struct platform_pwm_backlight_data gcw0_backlight_pdata = {
 	.polarity = PWM_POLARITY_NORMAL,
 	.max_brightness = 255,
 	.dft_brightness = 150,
-	.pwm_period_ns = 30000, /* 25 kHz: outside human hearing range */
+	.pwm_period_ns = 35000, /* 25 kHz: outside human hearing range */
 };
 
 static struct platform_device gcw0_backlight_device = {
